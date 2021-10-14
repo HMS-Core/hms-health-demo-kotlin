cd4d951acf8cb4da4025df134b86e15db9cefbb5
healthkit-hms <66779708+healthkit-hms@users.noreply.github.com>
2021-10-14 17:26:20 +0800

    Update HealthKitAuthActivity.kt
    add healthKit heartHealth scope

--- src/app/src/main/java/com/huawei/demo/health/auth/HealthKitAuthActivity.kt
+++ src/app/src/main/java/com/huawei/demo/health/auth/HealthKitAuthActivity.kt
@@ -93,7 +93,9 @@ class HealthKitAuthActivity : Activity() {
             // View and save the heart rate data in HUAWEI Health Kit.
             Scopes.HEALTHKIT_HEARTRATE_READ, Scopes.HEALTHKIT_HEARTRATE_WRITE,
             // View and save activityRecord in HUAWEI Health Kit.
-            Scopes.HEALTHKIT_ACTIVITY_RECORD_READ, Scopes.HEALTHKIT_ACTIVITY_RECORD_WRITE
+            Scopes.HEALTHKIT_ACTIVITY_RECORD_READ, Scopes.HEALTHKIT_ACTIVITY_RECORD_WRITE,
+            // View and save heartHealth data in HUAWEI Health Kit.
+            Scopes.HEALTHKIT_HEARTHEALTH_READ, Scopes.HEALTHKIT_HEARTHEALTH_WRITE
         )
 
         // Obtains the authorization process Intent.
